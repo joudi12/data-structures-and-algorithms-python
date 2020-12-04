ac078e873ac967c98ad8d519dac88db275e81ffa
joudi awameh <awamehj@gmail.com>
2020-12-05 00:31:16 +0200

    finish the code challeng5

--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,8 +1,49 @@
-from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
-    LinkedList,
-)
+from data_structures_and_algorithms.data_structures.linked_list.linked_list.linked_list import LinkedList
 
 
-def test_instance():
+
+
+
+def test_empty():
+    list = LinkedList()
+    assert list.head == None
+
+def test_insert_head():
     ll = LinkedList()
-    assert isinstance(ll, LinkedList)
+    ll.insert(5)
+    assert ll.head.value == 5
+def test_insert():
+    list = LinkedList()
+    list.insert(3)
+    assert list.head.value == 3
+def test_insert_multipal():
+    ll =LinkedList()
+    ll.insert(5)
+    ll.insert(7)
+    ll.insert(8)
+    assert ll.head.value ==8
+    assert ll.head.next.value ==7
+    assert ll.head.next.next.value ==5
+    assert ll.head.next.next.next ==None
+
+def test_include_true():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(7)
+    assert ll.includes(7)==True
+def test_include_false():
+    ll =LinkedList()
+    ll.insert(4)
+    ll.insert(9)
+    assert ll.includes(5) == False
+def test_string():
+    ll=LinkedList()
+    ll.insert(9)
+    ll.insert(7)
+    ll.insert(3)
+    assert ll.__str__()=="{ 3 } -> { 7 } -> { 9 } -> NULL"
+
+
+
+
+
